d7814d6f11f19d5492897309227675e1ff20e733
Nutthinun-526 <nutthinun75587@gmail.com>
2024-08-10 10:01:15 +0700

    Practice4_TextInput[10-08-2024]

--- components/Login.tsx
+++ components/Login.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Button, TextInput, TouchableOpacity } from "react-native";
+/* import { View, Text, Button, TextInput, TouchableOpacity } from "react-native";
 import React from "react";
 import { stylesLogin } from "../styles/styles";
 import { stylesPractice } from "../styles/styles";
@@ -33,4 +33,81 @@ function Login(): React.JSX.Element {
   );
 }
 
+export default Login;
+ */
+
+import { View, TextInput, Button, Alert } from "react-native";
+import React, { useState } from "react";
+import { stylesLogin } from "../styles/styles"; // เรียกใช้สไตล์จากไฟล์ styles.ts
+  const Login = (): React.JSX.Element => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const ValidityStateEmail = (email:string):boolean => {
+    const rechaeckEmail = /\S+@\S+\.\S+/
+    return rechaeckEmail.test(email);
+  }
+
+  const handleSubmit = () => {
+    let errorMessage = "";
+    if (!name) {
+      /* Alert.alert("Alert", "Please enter name.", [{ text: "OK" }]);
+      return; //break */
+      errorMessage += "Please enter Name\n";
+    }
+
+    //ตรวจสอบการป้อนรหัสผ่าน
+    if (!password) {
+      /* Alert.alert("Alert", "Please enter password.", [{ text: "OK" }]);
+      return; //break */
+      errorMessage += "Please enter Password\n";
+    }
+    else if(password.length<6){
+      errorMessage += "Password must be at lease 6 characters\n"
+    }
+
+    if (!email) {
+      /* Alert.alert("Alert", "Please enter email.", [{ text: "OK" }]);
+      return; //break */
+      errorMessage += "Please enter Email";
+    }
+    else if (!ValidityStateEmail(email)){
+      errorMessage += "Invalid Email format\n"
+    } 
+    if (errorMessage) {
+      Alert.alert("Error", errorMessage.trim(), [{ text: "OK" }]);
+      return; //break
+    }
+    Alert.alert("Alert", "Success", [{ text: "OK" }]);
+  };
+
+  return (
+    <View style={stylesLogin.container}>
+      <TextInput
+        style={stylesLogin.input}
+        placeholder="Enter Name"
+        value={name}
+        onChangeText={setName}
+      />
+      <TextInput
+        style={stylesLogin.input}
+        placeholder="Enter Email"
+        keyboardType="email-address"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={stylesLogin.input}
+        placeholder="Enter Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry={true}
+      />
+      <View>
+        <Button title="SUBMIT" onPress={handleSubmit} />
+      </View>
+    </View>
+  );
+};
 export default Login;
